2503d5d50e7b79610059e2f3c00277c383e0c3f7
Roberto <oliveirarobertw@gmail.com>
2025-01-30 22:02:13 -0300

    Adding coments

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd 
-import plotly.express as px
+import plotly.graph_objects as go
 
-st.set_page_config(layout='wide')
-df = pd.read_csv('data/googlestocks.csv', sep=',', decimal='.')
-df = df.dropna(axis=1, how='all')
+st.set_page_config(layout='wide') # Deixando a tela em modo wide
+df = pd.read_csv('data/googlestocks.csv', sep=',', decimal='.') # Importando os dados
+df = df.dropna(axis=1, how='all') # Removendo as colunas
+df['Date'] = pd.to_datetime(df['Date']) # Tirando a data de string para int
 
-df['Date'] = pd.to_datetime(df['Date'])
-df
